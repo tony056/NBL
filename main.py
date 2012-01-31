80d04e155c9a2b4671743fb1441917f35ebbb3fa
pcjeff <zendo3464@yahoo.com.tw>
2012-01-30 22:54:25 -0800

    test

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ class test2(webapp.RequestHandler):
     def get(self):
         template_values = {
             }
-        path = os.path.join(os.path.dirname(__file__), 'onMouse.txt')
+        path = os.path.join(os.path.dirname(__file__), 'test2.txt')
         self.response.out.write(template.render(path, template_values))    
     
 
